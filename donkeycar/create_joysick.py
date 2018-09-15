4a8dea35df39fce2ad0f59c3616c76da647f210b
Brychlikov <brychlikow@gmail.com>
2018-09-15 17:23:05 +0200

    Small fixes and improvements

--- donkeycar/create_joysick.py
+++ donkeycar/create_joysick.py
@@ -1,7 +1,8 @@
 from donkeycar.parts.controller import Joystick
 import json
 
-j = Joystick()
+j = Joystick('/dev/input/js0')
+j.init()
 button_fun_map = {}
 axis_fun_map = {}
 button_options = [
@@ -27,15 +28,28 @@ try:
         while not state and val != 1.0 and val != -1.0:
             button, state, axis, val = j.poll()
 
+        print(button, state, axis, val)
+
         if button and state:
             choice = input("what function to assign? Enter name or list index or 'l' to print the list")
-            while not (int(choice) in range(len(button_options)) or choice in button_options):
+            try:
+                int_choice = int(choice)
+            except:
+                int_choice = None
+            while not (int_choice in range(len(button_options)) or choice in button_options):
                 if choice == 'l':
                     print("Possible functions:")
                     for i, entry in enumerate(button_options):
                         print('{}. {}'.format(i, entry))
+                    choice = input("Choose an option")
                 else:
                     choice = input("Wrong option. Enter again")
+
+                try:
+                    int_choice = int(choice)
+                except:
+                    int_choice = None
+
             if len(choice) == 1:
                 choice = button_options[int(choice)]
 
@@ -43,36 +57,33 @@ try:
 
         elif axis and (val == 1.0 or val == -1.0):
             choice = input("what function to assign? Enter name or list index or 'l' to print the list")
-            while not (int(choice) in range(len(axis_options)) or choice in axis_options):
+
+            try:
+                int_choice = int(choice)
+            except:
+                int_choice = None
+
+            while not (int_choice in range(len(axis_options)) or choice in axis_options):
                 if choice == 'l':
                     print("Possible functions:")
                     for i, entry in enumerate(axis_options):
                         print('{}. {}'.format(i, entry))
+                    choice = input("Specify an option")
                 else:
                     choice = input("Wrong option. Enter again")
+
+                try:
+                    int_choice = int(choice)
+                except:
+                    int_choice = None
+
             if len(choice) == 1:
                 choice = axis_options[int(choice)]
+            axis_fun_map[axis] = choice
 except KeyboardInterrupt:
     print(button_fun_map)
     print(axis_fun_map)
-    with file('output.json', 'w') as f:
+    with open('output.json', 'w') as f:
         json.dump([button_fun_map, axis_fun_map], f)
 
 
-
-# Functions:
-
-# axis:
-# -throttle
-# -angle
-
-# buttons:
-# -change_mode
-# -record_toggle
-# -max_throttle_increase
-# -max_throttle_decrease
-# -throttle_scale_increase
-# -throttle_scale_decrease
-# -steering_scale_decrease
-# -steering_scale_increase
-# -constant_throttle_toggle
